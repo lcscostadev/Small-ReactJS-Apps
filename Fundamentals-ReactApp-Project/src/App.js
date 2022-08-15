f0959e6d9d5d219aadc7b1e1d2a9d4b5741ed55c
Lucas Costa <thelucascostadasilva@gmail.com>
2022-08-15 18:48:48 -0300

    Fundamentals React app that i've been doing to learn react fundamentals

---
+++ Fundamentals-ReactApp-Project/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Table from './Table';
+
+import './App.css';
+
+
+class App extends Component {
+
+    state = {
+        characters: [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Aspring actress',
+            },
+            {
+                name: 'Dennis',
+                job: 'Bartender',
+            },
+        ],
+
+    }
+
+    removeCharacter = (index) => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        })
+    }
+
+    render() {
+        const { characters } = this.state;
+
+        return (
+            <div className="container">
+                {/* characterData as an attribute, characters as his data */}
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
+            </div>
+        )
+    }
+}
+
+export default App;
