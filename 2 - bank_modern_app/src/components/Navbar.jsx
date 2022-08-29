fa30ea504b26a60a6aa08cbffccc82d10db166a2
Lucas Costa <thelucascostadasilva@gmail.com>
2022-08-29 20:04:34 -0300

    bank modern website using react and tailwind css

---
+++ 2 - bank_modern_app/src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+
+const Navbar = () => {
+
+    const [toggle, setToggle] = useState(false);
+
+    return (
+        <nav className='w-full flex py-6 justify-between items-center navbar'>
+            <img src={logo} alt="hoobank" className='w-[124px] h-[32px]' />
+
+            <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+                {navLinks.map((nav, index) => (
+                    // ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10' -> se for o último elemento do array, margin 0
+                    <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}>
+                        < a href={`#${nav.id}`}>
+                            {nav.title}
+                        </a>
+                    </li>
+                ))
+                }
+            </ul >
+
+            {/* Mobile Menu Section*/}
+            <div className='sm:hidden flex flex-1 justify-end items-center'>
+                {/* using prev to the previous version of that state making a callBack function*/}
+                <img src={toggle ? close : menu} alt="menu" className='w-[28px] h[28px] object-contain' onClick={() => setToggle((prev) => !prev)} />
+
+                {/* Menu */}
+                <div
+                    // if toggle is active then flex, if its not menu hidden
+                    className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+                >
+                    <ul className='list-none flex flex-col justify-end items-center flex-1'>
+                        {navLinks.map((nav, index) => (
+                            // ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10' -> se for o último elemento do array, margin 0
+                            <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}>
+                                < a href={`#${nav.id}`}>
+                                    {nav.title}
+                                </a>
+                            </li>
+                        ))
+                        }
+                    </ul >
+
+                </div>
+            </div>
+
+
+        </nav >
+    );
+}
+
+export default Navbar;
